e8ca8c2e32e986d4f72b02cf53a68cfb27587998
k.ueki <k.ueki@grv.jpn.com>
2019-11-27 13:24:28 +0900

    イベント処理書式を整える

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { ListGroup } from 'react-bootstrap';
+import {ListGroup} from 'react-bootstrap';
 import './menu.css';
 
 function handleClick(e) {
     e.preventDefault();
     console.log('The link was clicked.');
 }
+
 /*
 const Menu = () => {
     return <div className="menu">
@@ -24,25 +25,28 @@ class Menu extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            clicked: false,
+            renderChild: true,
         }
-        this.handleClick = this.handleClick.bind(this);
-        console.log('constructor now!');
+        this.handleUnmount = this.handleUnmount.bind(this);
+        console.log('コンストラクター 今!');
     }
-    handleClick() {
+
+    handleUnmount() {
         this.setState({
-            clicked: true,
-        })
+            renderChild: false,
+        });
     }
-    componentWillUnmount(){
+
+    componentWillUnmount() {
         console.log("コンポーネントがアンマウントされた後")
     }
+
     render() {
         return <div className="menu">
             <div>Menuです</div>
             <ListGroup>
                 <ListGroup.Item onClick={handleClick}>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item onClick={this.componentWillUnmount}>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item onClick={this.handleUnmount}>Dapibus ac facilisis in</ListGroup.Item>
                 <ListGroup.Item>Morbi leo risus</ListGroup.Item>
                 <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
                 <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
