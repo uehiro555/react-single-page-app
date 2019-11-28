73a205b69615803d2d2a6a6770356969337f1d2e
k.ueki <k.ueki@grv.jpn.com>
2019-11-28 13:17:33 +0900

    DOM操作サンプル実装

--- src/menu.js
+++ src/menu.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import {ListGroup} from 'react-bootstrap';
 import './menu.css';
+import ReactDOM from "react-dom";
+import Pickup from "./pickup";
 
 function handleClick(e) {
     e.preventDefault();
     console.log('The link was clicked.');
 }
 
-/*
-const Menu = () => {
+function onClick() {
+    ReactDOM.render(
+        <Content2/>,
+        document.querySelector('.content')
+    );
+}
+
+const Menu2 = () => {
     return <div className="menu">
-        <div>Menuです</div>
+        <div>Menu2です</div>
         <ListGroup>
-            <ListGroup.Item onClick={handleClick}>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
             <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
         </ListGroup>
     </div>
 };
-*/
+
+const Content2 = () => {
+    return <div className="content">
+        <Menu2/>
+        <Pickup/>
+    </div>
+};
+
 class Menu extends React.Component {
     constructor(props) {
         super(props);
@@ -46,7 +57,7 @@ class Menu extends React.Component {
             <div>Menuです</div>
             <ListGroup>
                 <ListGroup.Item onClick={handleClick}>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item onClick={this.handleUnmount}>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item onClick={onClick}>Dapibus ac facilisis in</ListGroup.Item>
                 <ListGroup.Item>Morbi leo risus</ListGroup.Item>
                 <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
                 <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
