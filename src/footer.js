6f8107d9bb09ce1feb92c8d0d07476d2112eada9
k.ueki <k.ueki@grv.jpn.com>
2019-11-29 11:14:05 +0900

    header footerのCSSの初期設定する

--- src/footer.js
+++ src/footer.js
@@ -3,8 +3,13 @@ import './footer.css';
 
 //Footerの定義
 const Footer = () => {
-    return <footer style={{textAlign: "center"}}>
-        <div>フッタです</div>
+    return <footer>
+        <div className="container sectionBox copySection text-center">
+            <p>Copyright © NavBar！ All Rights Reserved.</p><p>Powered by <a
+            href="https://wordpress.org/">WordPress</a> with <a href="https://lightning.nagoya/ja/" target="_blank"
+                                                                title="Free WordPress Theme Lightning"> Lightning
+            Theme</a> &amp; <a href="https://ex-unit.nagoya/ja/" target="_blank">VK All in One Expansion Unit</a> by <a
+            href="//www.vektor-inc.co.jp" target="_blank">Vektor,Inc.</a> technology.</p></div>
     </footer>
 
 };
--- src/header.js
+++ src/header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './header.css';
 
 //Headerの定義
 const Header = () => {
-    return <header style={{textAlign: "center"}}>
-        <h3>ヘッダです</h3>
+    return <header>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+        </Navbar>
     </header>
 };
 
